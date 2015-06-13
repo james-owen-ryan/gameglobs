23dbd32f12c5201c72c92bab0d6c14a57107807f
Eric Kaltman <ekaltman@gmail.com>
2015-06-13 16:58:56 -0700

    Changes: - Added in new URLs for mapping to UWSGI server - Add a .gitignore file

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -5,19 +5,19 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/gameglobs')
 def home():
     """Render the GameGlobs homepage."""
     return render_template('index.html')
 
 
-@app.route('/about')
+@app.route('/gameglobs/about')
 def about():
     """Render the about page."""
     return render_template('about.html')
 
 
-@app.route('/faq')
+@app.route('/gameglobs/faq')
 def faq():
     """Render the FAQ page."""
     return render_template('faq.html')
@@ -35,14 +35,14 @@ def faq():
 #     return render_template('viz.html', index_of_initial_k=index_of_initial_k)
 
 
-@app.route('/viz', methods=['POST'])
+@app.route('/gameglobs/viz', methods=['POST'])
 def viz():
     """Render the actual clustering visualization."""
     index_of_initial_k = request.form['indexOfSelectedKValue']
     return render_template('viz.html', index_of_initial_k=index_of_initial_k)
 
 
-@app.route('/viz')
+@app.route('/gameglobs/viz')
 def viz_without_pos():
     """In case someone tries to directly visit the viz URL, render the actual clustering visualization with k=2."""
     index_of_initial_k = 2
@@ -113,6 +113,6 @@ else:
 
 if not app.debug:
     import logging
-    file_handler = logging.FileHandler('gamenet.log')
+    file_handler = logging.FileHandler('gameglobs.log')
     file_handler.setLevel(logging.WARNING)
     app.logger.addHandler(file_handler)
